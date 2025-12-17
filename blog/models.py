e726d9b27e7b174a27252ea43de3441ef6a578e4
SParb <ryuchigo@gmail.com>
2025-12-17 13:16:44 +0000

    add create_post delete_post and edit post functions but currently not accessable via ui

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='blog_posts')
     featured_image = CloudinaryField('image', default='placeholder')
+    approved = models.BooleanField(default=False)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
